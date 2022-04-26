f21403c6f70f130ed7ffc7795d3725e1b103c745
BradBIerworth <bb300717@ohio.edu>
2022-04-26 11:08:00 -0400

    added countLine() function

--- counter.cc
+++ counter.cc
@@ -39,14 +39,39 @@ int main(int argc, char *argv[])
         cout << countLine(filename) << " Lines\n";
         cout << countChar(filename) << " Characters\n";
     }
-    
-    
- 
-    countLine(filename);
-    countChar(filename);
-
-    cout << countLine(filename) << " Lines\n";
-    cout << countChar(filename) << " Characters\n";
  
 	return 0;
+}
+
+
+/**
+ * @brief counts the number of lines in the text files
+ * 
+ * @return int count - the number of lines in the text file
+ */
+int countLine(string filename)
+{
+    string line;   // To read each line from code
+	int count=0;    // Variable to keep count of each line
+	
+	ifstream mFile (filename);   
+	
+	if(mFile.is_open()) 
+	{
+		while(mFile.peek()!=EOF)
+		{
+			getline(mFile, line);
+			count++;
+		}
+		mFile.close();
+	} else
+    {
+        for (int i = 0; i < filename.length(); i++)
+        {
+            getline(mFile, line);
+            count++;
+        }  
+    }
+    
+    return count;
 }
